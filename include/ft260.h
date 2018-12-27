d2283c151d1b7e0906c45d39186f8d5713ef498a
Pim van Pelt <pim@google.com>
2018-12-27 21:10:53 +0100

    Complete ft260.h with prototypes from Mongoose OS; Clean up includes a bt

--- include/ft260.h
+++ include/ft260.h
@@ -3,24 +3,18 @@
 #include <linux/types.h>
 #include <linux/input.h>
 #include <linux/hidraw.h>
-
+#include <libudev.h>
+#include <sys/ioctl.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-#include <stdio.h>
-
-#include <sys/ioctl.h>
-#include <libudev.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <errno.h>
 
 /*
  * For the systems that don't have the new version of hidraw.h in userspace.
@@ -98,5 +92,52 @@ bool ft260_i2c_read(struct ft260_dev *d, uint16_t addr, void *data, size_t len, 
  * performed.
  * If |stop| is true, then at the end of the operation bus will be released.
  */
-
 bool ft260_i2c_write(struct ft260_dev *d, uint16_t addr, const void *data, size_t len, bool stop);
+
+/*
+ * Release the bus (when left unreleased after read or write).
+ */
+void ft260_i2c_stop(struct ft260_dev *d);
+
+/*
+ * Helper for reading 1-byte register `reg` from a device at address `addr`.
+ * In case of success return a numeric byte value from 0x00 to 0xff; otherwise
+ * return -1.
+ */
+int ft260_i2c_read_reg_b(struct ft260_dev *d, uint16_t addr, uint8_t reg);
+
+/*
+ * Helper for reading 2-byte register `reg` from a device at address `addr`.
+ * In case of success returns a numeric big-endian value: e.g. if 0x01, 0x02
+ * was read from a device, 0x0102 will be returned.
+ *
+ * In case of error returns -1.
+ */
+int ft260_i2c_read_reg_w(struct ft260_dev *d, uint16_t addr, uint8_t reg);
+
+/*
+ * Helper for reading `n`-byte register value from a device. Returns true on
+ * success, false on error. Data is written to `buf`, which should be large
+ * enough.
+ */
+bool ft260_i2c_read_reg_n(struct ft260_dev *d, uint16_t addr, uint8_t reg, size_t n, uint8_t *buf);
+
+/*
+ * Helper for writing 1-byte register `reg` to a device at address `addr`.
+ * Returns `true` in case of success, `false` otherwise.
+ */
+bool ft260_i2c_write_reg_b(struct ft260_dev *d, uint16_t addr, uint8_t reg, uint8_t value);
+
+/*
+ * Helper for writing 2-byte register `reg` to a device at address `addr`.
+ * The value is big-endian: e.g. if `value` is `0x0102`, then `0x01, 0x02`
+ * will be written.
+ * Returns `true` in case of success, `false` otherwise.
+ */
+bool ft260_i2c_write_reg_w(struct ft260_dev *d, uint16_t addr, uint8_t reg, uint16_t value);
+
+/*
+ * Helper for writing `n`-byte register `reg` to a device at address `addr`.
+ * Returns `true` in case of success, `false` otherwise.
+ */
+bool ft260_i2c_write_reg_n(struct ft260_dev *d, uint16_t addr, uint8_t reg, size_t n, const uint8_t *buf);
