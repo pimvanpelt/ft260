8ec66a496c39bd64cbb2d1684ca94c76f99adf37
Pim van Pelt <pim@google.com>
2018-12-27 20:59:01 +0100

    Format with uncrustify

--- include/mgos_mock.h
+++ include/mgos_mock.h
@@ -3,26 +3,25 @@
 #include "mgos.h"
 
 // mgos_log
-enum cs_log_level
-{
-  LL_NONE = -1,
-  LL_ERROR = 0,
-  LL_WARN = 1,
-  LL_INFO = 2,
-  LL_DEBUG = 3,
+enum cs_log_level {
+  LL_NONE          = -1,
+  LL_ERROR         = 0,
+  LL_WARN          = 1,
+  LL_INFO          = 2,
+  LL_DEBUG         = 3,
   LL_VERBOSE_DEBUG = 4,
 
-  _LL_MIN = -2,
-  _LL_MAX = 5,
+  _LL_MIN          = -2,
+  _LL_MAX          = 5,
 };
 
 int log_print_prefix(enum cs_log_level l, const char *func, const char *file);
 
-#define LOG(l, x) \
-  do \
-  { \
-    if (log_print_prefix(l, __func__, __FILE__)) printf x; \
-    printf("\r\n"); \
+#define LOG(l, x)                                              \
+  do                                                           \
+  {                                                            \
+    if (log_print_prefix(l, __func__, __FILE__)) { printf x; } \
+    printf("\r\n");                                            \
   } while (0)
 
 
